38d130293b39c1e21ac5c1b9de62d69f80f2e704
Ivan <SolFly86@gmail.com>
2023-04-06 12:05:00 +0300

    Create chint-4.js

---
+++ chint-4.js
@@ -0,0 +1,16 @@
+const ModbusRTU = require("modbus-serial");
+
+const client = new ModbusRTU();
+client.connectTCP("192.168.0.101", { port: 4001 })
+    .then(function () {
+        console.log("Connected to MOXA");
+        return client.readInputRegisters(0x0100, 2, { unitId: 5 });
+    })
+    .then(function (data) {
+        console.log("Active Power:", data.data.readInt32BE());
+        client.close();
+    })
+    .catch(function (e) {
+        console.log("Error reading data from slave 5:", e.message);
+        client.close();
+    });

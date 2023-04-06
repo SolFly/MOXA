51d6bad1c6d0ab961d8400553b6926d3c0d367c7
Ivan <SolFly86@gmail.com>
2023-04-06 11:50:35 +0300

    Create chint-2.js

---
+++ chint-2.js
@@ -0,0 +1,54 @@
+const ModbusRTU = require('modbus-serial');
+
+const client = new ModbusRTU();
+
+client.connectTCP("192.168.0.101", { port: 4001 })
+  .then(() => {
+    console.log("Connected to MOXA");
+  })
+  .catch((err) => {
+    console.log("Connection error:", err);
+  });
+
+const registerStart = 3201; // Start address of the register
+const registerCount = 2; // Number of registers to read
+
+// Array to store the results
+const results = {};
+
+// Read data from multiple slaves
+for (let i = 1; i <= 30; i++) {
+  client.setID(i);
+  readData(i);
+}
+
+// Function to read data from a slave device
+function readData(slaveId) {
+  client.readInputRegisters(registerStart, registerCount)
+    .then((data) => {
+      const result = data.data.readInt32BE();
+      console.log(`Slave ${slaveId} - Result: ${result}`);
+      results[`TR${slaveId}`] = result;
+    })
+    .catch((err) => {
+      console.log(`Error reading data from slave ${slaveId}:`, err);
+      results[`TR${slaveId}`] = null;
+    });
+}
+
+// Serve the results on a web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('MOXA data is being read and served at /data');
+});
+
+app.get('/data', (req, res) => {
+  res.json(results);
+});
+
+app.listen(port, () => {
+  console.log(`Web server listening at http://localhost:${port}`);
+});
